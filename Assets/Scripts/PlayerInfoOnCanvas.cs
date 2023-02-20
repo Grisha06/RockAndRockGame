f4816cb298ec2c246d7712cfca1254436eb01862
NezertorcheaT <tigrisha06ps@gmail.com>
2023-02-20 13:00:37 +0300

    now there is no sprite distortion on the hp bar

--- Assets/Scripts/PlayerInfoOnCanvas.cs
+++ Assets/Scripts/PlayerInfoOnCanvas.cs
@@ -11,6 +11,7 @@ using TMPro;
 public class PlayerInfoOnCanvas : MonoCache
 {
     [SerializeField] private Image hp;
+    [SerializeField] private Image hp2;
     [SerializeField] private TextMeshProUGUI ammo;
     private async void Start()
     {
@@ -20,6 +21,10 @@ public class PlayerInfoOnCanvas : MonoCache
         ChangeHP(PlayerMover.single.hp);
         ChangeAmmo(PlayerMover.single.weapon[PlayerMover.single.weaponSelect].Ammo);
     }
-    private void ChangeHP(float hp_f) => hp.rectTransform.localScale = new Vector3(hp_f / PlayerMover.single.maxHealth, hp.rectTransform.localScale.y);
+    private void ChangeHP(float hp_f)
+    {
+        hp.rectTransform.localScale = new Vector3(hp_f / PlayerMover.single.maxHealth, hp.rectTransform.localScale.y);
+        hp2.rectTransform.localScale = new Vector3(hp.rectTransform.localScale.x > 0 ? 1 / (hp.rectTransform.localScale.x) : 0, hp2.rectTransform.localScale.y);
+    }
     private void ChangeAmmo(float ammo_f) => ammo.text = "Ammo = " + ammo_f.ToString();
 }
