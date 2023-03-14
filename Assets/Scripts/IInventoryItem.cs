3143e80aa9d6eab25b5882fa66ebd6a41a966e21
NezertorcheaT <tigrisha06ps@gmail.com>
2023-03-14 19:56:15 +0300

    inv

---
+++ Assets/Scripts/IInventoryItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyInventory
+{
+    public interface IInventoryItem
+    {
+        bool isEquipped { get; set; }
+        Type type { get; }
+        int maxItemsInInventorySlot { get; }
+        int amount { get; set; }
+
+        IInventoryItem Clone();
+    }
+
+    public interface IInventorySlot
+    {
+        bool isFull { get; }
+        bool isEmpty { get; }
+
+        IInventoryItem item { get; }
+        Type itemType { get; }
+        int amount { get; }
+        int capacity { get; }
+
+        void SetItem(IInventoryItem item);
+        void Clear();
+    }
+
+
+    public interface IInventory
+    {
+        int capacity { get; set; }
+        bool isFul { get; }
+
+        IInventoryItem GetItem(Type itemType);
+        IInventoryItem[] GetAllItems();
+        IInventoryItem[] GetAllItems(Type itemType);
+        IInventoryItem[] GetEquippedItems();
+        int GetItemAmount(Type itemType);
+
+        bool Add(object sender, IInventoryItem item);
+        bool HasItem(object sender, out IInventoryItem item);
+        bool Remove(object sender, IInventoryItem item, int amount = 1);
+
+    }
+}
